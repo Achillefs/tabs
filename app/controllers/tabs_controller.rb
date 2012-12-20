99c1912d82dad1e75b876e575a5e976eedb13e5e
Achilles Charmpilas <ac@humbuckercode.co.uk>
2012-12-20 04:26:49 +0100

    create method

--- app/controllers/tabs_controller.rb
+++ app/controllers/tabs_controller.rb
@@ -1,5 +1,15 @@
 class TabsController < ApplicationController
   def new
-    
+    @tab = Tab.new
+  end
+  
+  def create
+    @tab = Tab.new(params[:tab])
+    if @tab.save
+      redirect_to tab_path(@tab)
+    else
+      flash[:error] = 'Curses! Curses? Yah. Something went wrong'
+      render :new
+    end
   end
 end
--- spec/controllers/tabs_controller_spec.rb
+++ spec/controllers/tabs_controller_spec.rb
@@ -5,5 +5,20 @@ describe TabsController do
     before { get :new }
     
     it { response.code.to_i.should eq(200) }
+    it { assigns(:tab).should be_a_new(Tab) }
+  end
+  
+  describe '#create' do
+    before { post :create, build(:tab).attributes }
+    
+    it { response.code.to_i.should eq(302) }
+    it { assigns(:tab).should eq(Tab.first) }
+    
+    it "should not allow dupes" do
+      post :create, build(:tab).attributes
+      response.code.to_i.should eq(200)
+      flash[:error].should_not eq(nil)
+    end
+    
   end
 end
